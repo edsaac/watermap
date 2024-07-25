5014922fe682e59e856b5a3dbfd920d6c3e4f1cc
edsaa <esaavedrac@u.northwestern.edu>
2024-07-25 09:14:27 -0500

    To parent element

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,3 @@
-var container = window.parent.document.parentElement.document.getElementById("root");
+var container = window.parent.document.parent.document.getElementById("root");
 container.getElementsByClassName("viewerBadge_link__qRIco")[0].style.display = "none";
 console.log(container);
